14c32e631abad3048340d5974237958bc1059c23
Orijadribo <danielorijadribo@gmail.com>
2024-07-12 00:20:02 +0300

    Dummy date

--- src/constants/index.js
+++ src/constants/index.js
@@ -5,7 +5,7 @@ export const articles = [
     title: 'ADOPTION OF THE REVISED CONSTITUTION',
     details: [
       {
-        id: 101,
+        id: 1.1,
         content:
           'At the Annual General Meeting of West Nile Club held on ………… 2024, this Revised Constitution was adopted by resolution of the members of West Nile Club.',
       },
@@ -17,7 +17,7 @@ export const articles = [
     title: 'DEFINITIONS',
     details: [
       {
-        id: 201,
+        id: 2.1,
         content:
           'This section defines key terms used in the constitution, including calendar year, club, constitution, committees, elected officers, meetings, golf course, members, patron, and trustees.',
       },
@@ -29,31 +29,31 @@ export const articles = [
     title: 'ESTABLISHMENT AND COLOURS OF THE CLUB',
     details: [
       {
-        id: 301,
+        id: 3.1,
         content:
           'There is established a club known as “West Nile Club” hereinafter referred to as “the Club.”     ',
       },
       {
-        id: 302,
+        id: 3.2,
         content: 'The Club shall be a Members’ only club.',
       },
       {
-        id: 303,
+        id: 3.3,
         content:
           'The Club shall be a voluntary social association of Members and a non-political body.',
       },
       {
-        id: 304,
+        id: 3.4,
         content:
           'The headquarters of the Club shall be situated at Plot No. 59 Weatherhead Park Lane, Arua City, Uganda.',
       },
       {
-        id: 305,
+        id: 3.5,
         content:
           'The official colours of the Club shall be………………………………, Uganda.',
       },
       {
-        id: 306,
+        id: 3.6,
         content:
           'The emblem of the Club shall be as set out in the schedule to this constitution.',
       },
