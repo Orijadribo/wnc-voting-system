1f5a8da068f9ac3c1ed2444db89630ba1a4deba5
Orijadribo <danielorijadribo@gmail.com>
2024-07-12 08:47:02 +0300

    reduced size of text on login

--- src/pages/Login.jsx
+++ src/pages/Login.jsx
@@ -13,15 +13,15 @@ const Login = () => {
     >
       <div className='flex flex-col items-center justify-center rounded-lg w-[350px] md:w-[400px] bg-[#FFFFFF]/[0.8] p-10 md:p-14 shadow-xl'>
         <div className='flex flex-col items-center justify-center'>
-          <img className='w-32' src={WNGC_logo} alt='LSGA Logo' />
+          <img className='w-32' src={WNGC_logo} alt='WNGC_logo' />
           {/* <img className='w-32' src={course} alt='WNGC Logo' /> */}
           <h1 className='py-5 font-light text-center text-[32px] w-full'>
             WNC Constitution Voter's Login
           </h1>
         </div>
 
-        <div className='flex items-center justify-between w-full pb-5'>
-          <a className='text-sm underline' href=''>
+        <div className='flex gap-4 items-center justify-between w-full pb-5'>
+          <a className='text-[12px] underline' href=''>
             Forgot your password?
           </a>
           <div className='flex items-center justify-center'>
@@ -31,14 +31,16 @@ const Login = () => {
               name='remember'
               value='Remember me'
             />
-            <label className='pl-2 text-sm' htmlFor='remember'>
+            <label className='pl-2 text-[12px]' htmlFor='remember'>
               Remember me?
             </label>
           </div>
         </div>
 
         <Link to='/section_one'>
-          <button className=' border py-2 px-5 mt-5 rounded-lg bg-green-50 text-lg'>Verify Subscription</button>
+          <button className=' border py-2 px-5 mt-5 rounded-lg bg-green-50 text-lg'>
+            Verify Subscription
+          </button>
         </Link>
       </div>
     </div>
