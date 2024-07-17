edbc404b349adbcecc0dfb088e18ff4836916c94
Orijadribo <danielorijadribo@gmail.com>
2024-07-17 12:19:17 +0300

    button to continue to login

--- src/pages/login/VerificationModal.jsx
+++ src/pages/login/VerificationModal.jsx
@@ -1,18 +1,39 @@
 import React, { useState } from 'react';
+import { Link } from 'react-router-dom';
 
 const VerificationModal = ({ firstName, lastName }) => {
-  const [verified, setVerified] = useState(false);
+  const [verified, setVerified] = useState(true);
 
   return (
     <div className='fixed top-0 left-0 w-full h-full flex items-center justify-center bg-gray-800 bg-opacity-75'>
       <div className='bg-white p-5 md:p-10 w-[90%] lg:w-[50%] rounded-lg flex flex-col gap-5'>
         {verified ? (
-          <div>user is verified</div>
+          <div className='flex items-center justify-center'>
+            <div className='flex flex-col gap-3 items-center justify-center capitalize text-center '>
+              <div className='flex flex-col text-green-600'>
+                <span className='font-bold text-black'>
+                  {firstName} {lastName}
+                </span>{' '}
+                you are paid up
+              </div>
+              <div>
+                <p className=''>continue to the login page</p>
+                <p className='text-sm'>
+                  (please enter the credentials provided to you)
+                </p>
+              </div>
+              <Link to='/login'>
+                <button className='border py-2 px-5 mt-5 rounded-lg bg-green-50 text-lg'>
+                  continue
+                </button>
+              </Link>
+            </div>
+          </div>
         ) : (
           <div className='flex items-center justify-center'>
-            <div className='flex flex-col gap-5 items-center justify-center capitalize text-center'>
-              <div>
-                <span className='font-bold'>
+            <div className='flex flex-col gap-3 items-center justify-center capitalize text-center'>
+              <div className='flex flex-col text-red-600'>
+                <span className='font-bold text-black'>
                   {firstName} {lastName}
                 </span>{' '}
                 you are not paid up
