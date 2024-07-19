1bdb18d0201c313063ccf3d84aaf8ae57e29f9e4
Orijadribo <danielorijadribo@gmail.com>
2024-07-19 20:11:11 +0300

    SideBar open and close

--- src/pages/admin/SideBar.jsx
+++ src/pages/admin/SideBar.jsx
@@ -1,36 +1,62 @@
 import React, { useState } from 'react';
 
 const SideBar = ({ setSelectedScreen }) => {
+  const [isOpen, setIsOpen] = useState(true);
+
   const handleScreenSelection = (screen) => {
     setSelectedScreen(screen);
     // setSideBar(!sideBar);
   };
 
+  const handleCloseSideBar = () => {
+    setIsOpen(!isOpen);
+  };
+
   return (
-    <div className='flex flex-col gap-3 py-5 px-5 font-light'>
-      <div
-        className='cursor-pointer pl-4 py-1 hover:bg-green-100 rounded-md'
-        onClick={() => handleScreenSelection('home')}
-      >
-        Home
+    <div className='flex flex-col justify-between border h-full'>
+      <div className='flex flex-col gap-3 py-5 px-2 mx-2 md:px-5 font-light'>
+        <div
+          className='flex gap-2 items-center cursor-pointer px-2 md:pl-4 py-1 hover:bg-green-100 rounded-md'
+          onClick={() => handleScreenSelection('home')}
+        >
+          <div className='w-5 h-5 border'></div>
+          <div className={`${isOpen ? 'hidden' : 'block'} md:block`}>Home</div>
+        </div>
+        <div
+          className='flex gap-2 items-center cursor-pointer px-2 md:pl-4 py-1 hover:bg-green-100 rounded-md'
+          onClick={() => handleScreenSelection('users')}
+        >
+          <div className='w-5 h-5 border'></div>
+          <div className={`${isOpen ? 'hidden' : 'block'} md:block`}>Users</div>
+        </div>
+        <div
+          className='flex gap-2 items-center cursor-pointer px-2 md:pl-4 py-1 hover:bg-green-100 rounded-md'
+          onClick={() => handleScreenSelection('votes')}
+        >
+          <div className='w-5 h-5 border'></div>
+          <div className={`${isOpen ? 'hidden' : 'block'} md:block`}>Votes</div>
+        </div>
+        <div
+          className='flex gap-2 items-center cursor-pointer px-2 md:pl-4 py-1 hover:bg-green-100 rounded-md'
+          onClick={() => handleScreenSelection('report')}
+        >
+          <div className='w-5 h-5 border'></div>
+          <div className={`${isOpen ? 'hidden' : 'block'} md:block`}>
+            Report
+          </div>
+        </div>
       </div>
-      <div
-        className='cursor-pointer pl-4 py-1 hover:bg-green-100 rounded-md'
-        onClick={() => handleScreenSelection('users')}
-      >
-        Users
-      </div>
-      <div
-        className='cursor-pointer pl-4 py-1 hover:bg-green-100 rounded-md'
-        onClick={() => handleScreenSelection('votes')}
-      >
-        Votes
-      </div>
-      <div
-        className='cursor-pointer pl-4 py-1 hover:bg-green-100 rounded-md'
-        onClick={() => handleScreenSelection('report')}
-      >
-        Report
+
+      <div className='black md:hidden'>
+        <div className='flex flex-col gap-3 py-5 px-2 mx-2 md:px-5 font-light'>
+          <div
+            className='flex gap-2 items-center cursor-pointer px-2 md:pl-4 py-1 hover:bg-green-100 rounded-md'
+            onClick={handleCloseSideBar}
+          >
+            <div className='w-5 h-5 border'></div>
+            <div className={`${isOpen ? 'hidden' : 'block'}`}>Close</div>
+          </div>
+        </div>
       </div>
     </div>
   );
