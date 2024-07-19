920e4f5178a6747d7fcf8f044a8c1278676ce348
Orijadribo <danielorijadribo@gmail.com>
2024-07-19 20:10:57 +0300

    Home page

--- src/pages/admin/content/Home.jsx
+++ src/pages/admin/content/Home.jsx
@@ -5,23 +5,66 @@ const Home = () => {
     <div className=''>
       <div className='mb-5 text-2xl'>Home</div>
       <div
-        className='flex flex-col gap-5 overflow-y-auto'
+        className='flex flex-col gap-5 overflow-y-auto rounded-lg'
         style={{ height: 'calc(100vh - 220px)' }}
       >
-        <div className='flex gap-5'>
-          <div className='flex-[2] bg-green-50/80 rounded-lg shadow-sm h-56 cursor-pointer'>
-            Active users
+        <div className='flex flex-col md:flex-row gap-5'>
+          <div className='flex-[2] bg-green-50/80 rounded-lg shadow-sm cursor-pointer relative'>
+            <div className='sticky top-0 py-2 px-5 bg-white/90'>
+              Active users
+            </div>
+            <div className='py-2 px-5'>
+              <div className=''>Active users</div>
+              <div>Active users</div>
+              <div>Active users</div>
+              <div>Active users</div>
+              <div>Active users</div>
+              <div>Active users</div>
+              <div>Active users</div>
+              <div>Active users</div>
+              <div>Active users</div>
+              <div>Active users</div>
+              <div>Active users</div>
+              <div>Active users</div>
+              <div>Active users</div>
+              <div>Active users</div>
+              <div>Active users</div>
+              <div>Active users</div>
+            </div>
           </div>
-          <div className='flex-[2] bg-green-50/80 rounded-lg shadow-sm cursor-pointer'>
-            Previous Voters
+          <div className='flex-[2] bg-green-50/80 rounded-lg shadow-sm cursor-pointer relative'>
+            <div className='sticky top-0 py-2 px-5 bg-white/90'>
+              Previous Voters
+            </div>
+            <div className='py-2 px-5'>
+              <div>Daniel</div>
+              <div>Daniel</div>
+              <div>Daniel</div>
+              <div>Daniel</div>
+            </div>
           </div>
-          <div className='flex-[2] bg-green-50/80 rounded-lg shadow-sm cursor-pointer'>
-            Yet to vote
+          <div className='flex-[2] bg-green-50/80 rounded-lg shadow-sm cursor-pointer relative'>
+            <div className='sticky top-0 py-2 px-5 bg-white/90'>
+              Yet to vote
+            </div>
+            <div className='py-2 px-5'>
+              <div>Orija</div>
+              <div>Orija</div>
+              <div>Orija</div>
+              <div>Orija</div>
+            </div>
           </div>
         </div>
         <div>
           <div className='bg-green-50/80 rounded-lg shadow-sm h-40'>
-            Votes each day
+            <div className='sticky top-0 py-2 px-5 bg-white/90'>
+              Votes each day
+            </div>
+            <div className='py-2 px-5'>
+              <div>Orija</div>
+              <div>Orija</div>
+              <div>Orija</div>
+            </div>
           </div>
         </div>
       </div>
