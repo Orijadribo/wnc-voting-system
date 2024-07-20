8d04efaa16ccd9197479270bbf6ab6c10ade219f
Orijadribo <danielorijadribo@gmail.com>
2024-07-20 21:49:02 +0300

    Votes component

--- src/pages/admin/content/votes/Votes.jsx
+++ src/pages/admin/content/votes/Votes.jsx
@@ -1,9 +1,88 @@
-import React from 'react'
+import React from 'react';
 
 const Votes = () => {
-  return (
-    <div>Votes</div>
-  )
-}
+  const votes = [
+    {
+      article: 'Article 1',
+      yes: 10,
+      no: 20,
+    },
+    {
+      article: 'Article 2',
+      yes: 30,
+      no: 5,
+    },
+    {
+      article: 'Article 3',
+      yes: 20,
+      no: 20,
+    },
+    {
+      article: 'Article 4',
+      yes: 0,
+      no: 40,
+    },
+    {
+      article: 'Article 5',
+      yes: 15,
+      no: 0,
+    },
+    {
+      article: 'Article 6',
+      yes: 10,
+      no: 0,
+    },
+    {
+      article: 'Article 7',
+      yes: 0,
+      no: 40,
+    },
+    {
+      article: 'Article 8',
+      yes: 15,
+      no: 0,
+    },
+    {
+      article: 'Article 9',
+      yes: 10,
+      no: 0,
+    },
+    {
+      article: 'Article 10',
+      yes: 0,
+      no: 40,
+    },
+    {
+      article: 'Article 11',
+      yes: 15,
+      no: 0,
+    },
+    {
+      article: 'Article 12',
+      yes: 10,
+      no: 0,
+    },
+  ];
 
-export default Votes
+  return (
+    <div className=''>
+      <div className='mb-5 text-2xl'>Votes</div>
+      <div
+        className='grid grid-cols-1 md:grid-cols-3 gap-5 overflow-y-auto rounded-lg'
+        style={{ height: 'calc(100vh - 220px)' }}
+      >
+        {votes.map((article) => (
+          <div className='rounded-lg shadow-sm bg-green-50/80 h-40  py-2 px-5'>
+            <div className='text-xl'>{article.article}</div>
+            <div>
+              <div>{article.yes}</div>
+              <div>{article.no}</div>
+            </div>
+          </div>
+        ))}
+      </div>
+    </div>
+  );
+};
+
+export default Votes;
