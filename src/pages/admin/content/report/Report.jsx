b0a66cbbcc29072ee7d96f1ec2547c70e3e934d3
Orijadribo <danielorijadribo@gmail.com>
2024-07-24 15:53:14 +0300

    Good here

--- src/pages/admin/content/report/Report.jsx
+++ src/pages/admin/content/report/Report.jsx
@@ -81,6 +81,114 @@ const Report = () => {
               Result Summary
             </p>
             <div className='border'>
+              <div className='flex flex-col gap-2 my-5 p-5'>
+                <p className='text-[28px] pb-5'>Article 1</p>
+                <div className='flex px-5 pb-5'>
+                  <div className='flex flex-1 flex-col'>
+                    <p>Voted Yes: 20</p>
+                    <p>Voted No: 10</p>
+                  </div>
+                  <div className='flex-[3]'>pie chart</div>
+                </div>
+                <div className='flex flex-col gap-2'>
+                  <p className='text-[20px]'>Reasons For No Vote</p>
+                  <div className='flex flex-col gap-2'>
+                    <div className='flex'>
+                      <p className='flex-1 flex items-center justify-start'>
+                        Daniel
+                      </p>
+                      <p className='flex-[4] text-justify'>
+                        Lorem ipsum dolor sit amet, consectetur adipisicing
+                        elit. Illum sed vel nostrum nesciunt saepe repudiandae
+                        porro officia animi, ipsum magnam.
+                      </p>
+                    </div>
+                    <hr className=' border' />
+                  </div>
+                  <div className='flex flex-col gap-2'>
+                    <div className='flex'>
+                      <p className='flex-1 flex items-center justify-start'>
+                        Orija
+                      </p>
+                      <p className='flex-[4] text-justify'>
+                        Lorem ipsum, dolor sit amet consectetur adipisicing
+                        elit. Earum odio ipsa pariatur sunt fuga distinctio
+                        voluptas itaque ex consequuntur iure in natus voluptate
+                        beatae magnam, hic dolorem libero amet! Et.
+                      </p>
+                    </div>
+                    <hr className=' border' />
+                  </div>
+                  <div className='flex flex-col gap-2'>
+                    <div className='flex'>
+                      <p className='flex-1 flex items-center justify-start'>
+                        peti
+                      </p>
+                      <p className='flex-[4] text-justify'>
+                        Lorem ipsum, dolor sit amet consectetur adipisicing
+                        elit. Est, dicta?
+                      </p>
+                    </div>
+                    <hr className=' border' />
+                  </div>
+                </div>
+              </div>
+            </div>
+            <div className='border'>
+              <div className='flex flex-col gap-2 p-5'>
+                <p className='text-[28px]'>Article 1</p>
+                <div className='flex px-5'>
+                  <div className='flex flex-1 flex-col'>
+                    <p>Voted Yes: 20</p>
+                    <p>Voted No: 10</p>
+                  </div>
+                  <div className='flex-[3]'>pie chart</div>
+                </div>
+                <div className='flex flex-col gap-2'>
+                  <p className='text-[20px]'>Reasons For No Vote</p>
+                  <div className='flex flex-col gap-2'>
+                    <div className='flex'>
+                      <p className='flex-1 flex items-center justify-start'>
+                        Daniel
+                      </p>
+                      <p className='flex-[4] text-justify'>
+                        Lorem ipsum dolor sit amet, consectetur adipisicing
+                        elit. Illum sed vel nostrum nesciunt saepe repudiandae
+                        porro officia animi, ipsum magnam.
+                      </p>
+                    </div>
+                    <hr className=' border' />
+                  </div>
+                  <div className='flex flex-col gap-2'>
+                    <div className='flex'>
+                      <p className='flex-1 flex items-center justify-start'>
+                        Orija
+                      </p>
+                      <p className='flex-[4] text-justify'>
+                        Lorem ipsum, dolor sit amet consectetur adipisicing
+                        elit. Earum odio ipsa pariatur sunt fuga distinctio
+                        voluptas itaque ex consequuntur iure in natus voluptate
+                        beatae magnam, hic dolorem libero amet! Et.
+                      </p>
+                    </div>
+                    <hr className=' border' />
+                  </div>
+                  <div className='flex flex-col gap-2'>
+                    <div className='flex'>
+                      <p className='flex-1 flex items-center justify-start'>
+                        peti
+                      </p>
+                      <p className='flex-[4] text-justify'>
+                        Lorem ipsum, dolor sit amet consectetur adipisicing
+                        elit. Est, dicta?
+                      </p>
+                    </div>
+                    <hr className=' border' />
+                  </div>
+                </div>
+              </div>
+            </div>
+            <div className='border'>
               <div className='flex flex-col gap-2 p-5'>
                 <p className='text-[28px]'>Article 1</p>
                 <div className='flex px-5'>
