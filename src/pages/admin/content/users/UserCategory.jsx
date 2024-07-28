009d4dde513a51ea62e54463af46a8e9a5a310aa
Orijadribo <danielorijadribo@gmail.com>
2024-07-28 15:21:59 +0300

    Removed redundant code

--- src/pages/admin/content/users/UserCategory.jsx
+++ src/pages/admin/content/users/UserCategory.jsx
@@ -3,15 +3,13 @@ import AddMemberModal from './AddMemberModal';
 import { DataGrid } from '@mui/x-data-grid';
 
 const UserCategory = ({ isVisible, setIsVisible, data, selection }) => {
-
   const columns = [
     { field: 'id', headerName: 'ID', width: 70 },
     { field: 'firstName', headerName: 'First name', width: 130 },
-    { field: 'lastName', headerName: 'Last name', width: 130 },    
+    { field: 'lastName', headerName: 'Last name', width: 130 },
     {
       field: 'userName',
       headerName: 'Username',
-      description: 'This column has a value getter and is not sortable.',
       sortable: false,
       width: 160,
     },
@@ -20,10 +18,11 @@ const UserCategory = ({ isVisible, setIsVisible, data, selection }) => {
   return (
     <div className='w-[235px] md:w-full'>
       <div className='relative rounded-lg'>
-        <div className='rounded-lg border' style={{ height: 400, width: '100%' }}>
-          <div style={{ height: 400,  }}>
-            {' '}
-            {/* Ensure the DataGrid's container takes full width */}
+        <div
+          className='rounded-lg border'
+          style={{ height: 400, width: '100%' }}
+        >
+          <div style={{ height: 400 }}>
             <DataGrid
               rows={data}
               columns={columns}
